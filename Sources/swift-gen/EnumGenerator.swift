efa910363a53f9273108529dfed39407085ad1e6
Arror <hallo.maqiang@gmail.com>
2019-06-01 15:04:16 +0800

    Update enum struct gen

--- Sources/swift-gen/EnumGenerator.swift
+++ Sources/swift-gen/EnumGenerator.swift
@@ -25,21 +25,22 @@ class EnumGenerator {
         let values = e.values.values.sorted { lhs, rhs in
             return lhs.value < rhs.value
         }
-        let accessControl: String = (scope == .client) ? "public " : ""
-        p.print("\(accessControl)enum \(scope.prefix)\(e.name): Int, Codable, CaseIterable {\n")
+        let control: String = (scope == .client) ? "public " : ""
+        p.print("\(control)enum \(scope.prefix)\(e.name): Int, Codable, CaseIterable {\n")
         p.indent()
         values.forEach { v in
             p.print("case \(v.name) = \(v.value)\n")
         }
-        self.generateEnumInit(scope: scope, values: values, printer: &p)
+        if scope == .client {
+            self.generateEnumInit(values: values, printer: &p)
+        }
         p.outdent()
         p.print("}\n")
         p.print("\n")
     }
     
-    private func generateEnumInit(scope: Scope, values: [TEnumValue], printer p: inout CodePrinter) {
-        let accessControl: String = (scope == .client) ? "public " : ""
-        p.print("\(accessControl)init?(rawValue: Int) {\n")
+    private func generateEnumInit(values: [TEnumValue], printer p: inout CodePrinter) {
+        p.print("public init?(rawValue: Int) {\n")
         p.indent()
         p.print("switch rawValue {\n")
         values.forEach { v in
--- Sources/swift-gen/StructGenerator.swift
+++ Sources/swift-gen/StructGenerator.swift
@@ -22,21 +22,22 @@ class StructGenerator {
     }
     
     private func generateStruct(scope: Scope,  s: TStruct, printer p: inout CodePrinter) {
-        let accessControl: String = (scope == .client) ? "public " : ""
-        p.print("\(accessControl)struct \(scope.prefix)\(s.name): Codable {\n")
+        let control: String = (scope == .client) ? "public " : ""
+        p.print("\(control)struct \(scope.prefix)\(s.name): Codable {\n")
         p.indent()
         s.fields.forEach { field in
-            p.print("\(accessControl)let \(field.name): \(field.generateSwiftTypeName(scope: scope))\n")
+            p.print("\(control)let \(field.name): \(field.generateSwiftTypeName(scope: scope))\n")
         }
-        self.generateStructInit(scope: scope, values: s.fields, printer: &p)
+        if scope == .client {
+            self.generateStructInit(scope: scope, values: s.fields, printer: &p)
+        }
         p.outdent()
         p.print("}\n")
         p.print("\n")
     }
     
     private func generateStructInit(scope: Scope, values: [TField], printer p: inout CodePrinter) {
-        let accessControl: String = (scope == .client) ? "public " : ""
-        p.print("\(accessControl)init(\(values.map({ "\($0.name): \($0.generateSwiftTypeName(scope: scope))" }).joined(separator: ", "))) {\n")
+        p.print("public init(\(values.map({ "\($0.name): \($0.generateSwiftTypeName(scope: scope))" }).joined(separator: ", "))) {\n")
         p.indent()
         values.forEach { v in
             p.print("self.\(v.name) = \(v.name)\n")
